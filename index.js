466846df91a03b34403971c8d24f2360ad610523
ravshan01 <ravshanselimov1@gmail.com>
2020-12-30 22:44:03 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 module.exports = function isEmpty(variable){
 
-  if ( Array.isArray(variable) || typeof variable === 'string' )
-    return variable.length === 0;
+  if ( Array.isArray(variable) || typeof variable === 'string' ){
+    return variable.length === 0
+  }
+  if ( typeof variable === 'object' && variable !== null ){
+    return Object.keys(variable).length === 0
+  }
 
-  if ( typeof variable === 'object' && variable !== null )
-    return Object.keys(variable).length === 0;
+  if ( typeof variable === 'number' ){
+    return variable >= 1 ? false : true
+  }
+  if ( typeof variable === 'boolean' ){
+    return !variable
+  }
+  if ( variable === undefined || variable === null || variable === NaN ){
+    return true
+  }
 
-  if ( typeof variable === 'number' )
-     return variable >= 1 ? false : true;
 
-  if ( typeof variable === 'boolean' )
-    return !variable;
-
-   return true
+  throw new Error('this type not supported');
 }
