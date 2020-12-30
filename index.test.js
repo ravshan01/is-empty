dae9acf9ff584b05d1740a0dd0070c2541d47fc6
ravshan01 <ravshanselimov1@gmail.com>
2020-12-30 22:10:38 +0500

    Create index.test.js

---
+++ index.test.js
@@ -0,0 +1,28 @@
+const isEmpty = require('./index');
+
+
+test('empty object', () => expect( isEmpty({}) ).toBe(true) );
+test('data object',  () => expect( isEmpty({ test: [] }) ).toBe(false) );
+
+test('empty array', () => expect( isEmpty([]) ).toBe(true) );
+test('data array',  () => expect( isEmpty([ 1, '2', null ]) ).toBe(false) );
+
+test('empty string', () => expect( isEmpty('') ).toBe(true) );
+test('data string',  () => expect( isEmpty('Hello World') ).toBe(false) );
+
+test('empty map', () => expect( isEmpty(new Map()) ).toBe(true) );
+test('data map',  () => expect( isEmpty(new Map([ ['data', 'Hello World'] ])) ).toBe(false) );
+
+test('empty set', () => expect( isEmpty(new Set()) ).toBe(true) );
+test('data set',  () => expect( isEmpty( new Set([ 'Hello', 'World' ]) ) ).toBe(false) );
+
+test('negative number', () => expect( isEmpty(-5) ).toBe(true) );
+test('zero number',     () => expect( isEmpty(0) ).toBe(true) );
+test('positive number', () => expect( isEmpty(5) ).toBe(false) );
+
+test('false boolean', () => expect( isEmpty(false) ).toBe(true) );
+test('true boolean',  () => expect( isEmpty(true) ).toBe(false) );
+
+test('undefined', () => expect( isEmpty(undefined) ).toBe(true) );
+test('null', () => expect( isEmpty(null) ).toBe(true) );
+test('Nan', () => expect( isEmpty(NaN) ).toBe(true) );
