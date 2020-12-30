7fb3cfecdb59b670f1a781f03cf1f2d8b1849cb2
ravshan01 <ravshanselimov1@gmail.com>
2020-12-30 21:51:24 +0500

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-export default function isEmpty(variable){
+module.exports = function isEmpty(variable){
 	switch (typeof variable) {
 
-		case 'variableect'  : return variableect.keys(variable).length === 0
+		case 'variable'  : return Object.keys(variable).length === 0
 		case 'array'   : return variable.length === 0
 		case 'string'  : return variable.length === 0
     case ('map' || 'set') : return variable.size >= 1 ? false : true
-    
+
 		case 'number'  : return variable >= 1 ? false : true
-		case 'boolean' : return variable
+		case 'boolean' : return !variable
 
     // undefined, null, NaN
     default : return false
