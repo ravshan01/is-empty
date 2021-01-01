34a1019bc0093bb49eb29c84ad30e55a9ab584ea
ravshan01 <ravshanselimov1@gmail.com>
2021-01-01 20:06:19 +0500

    v0.0.2
    package name - isEmpty=>@ravshan01/isEmpty
    implemented checking for the following types: map, set, file, funtcion

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 module.exports = function isEmpty(variable){ 
 
-  if ( Array.isArray(variable) || typeof variable === 'string' ){
-    return variable.length === 0
-  }
-  if ( typeof variable === 'object' && variable !== null ){
-    return Object.keys(variable).length === 0
+  if ( typeof variable === 'number' ) return variable >= 1 ? false : true;
+  if ( typeof variable === 'string' ) return variable.length === 0; 
+  if ( typeof variable === 'boolean' ) return !variable;
+  if ( typeof variable === 'function' ) return variable.length === 0;
+  if ( typeof variable === 'symbol' ) return isEmpty(variable.description);
+  if ( Array.isArray(variable) ) return variable.length === 0
+
+  // Objects...
+  if (variable?.toString === Object.prototype.toString ){
+  	switch ( variable.toString() ){
+
+  		case '[object File]' :
+  		case '[object Map]' :
+  		case '[object Set]' : {
+  			return variable.size === 0
+  		}
+
+  		case '[object Object]' : {
+  			return Object.keys(variable).length === 0
+  		}
+
+  	}
   }
 
-  if ( typeof variable === 'number' ){
-    return variable >= 1 ? false : true
-  }
-  if ( typeof variable === 'boolean' ){
-    return !variable
-  }
-  if ( typeof variable === 'symbol' ){
-    return isEmpty(variable.description)
-  }
 
-  if ( variable === undefined || variable === null || variable === NaN ){
-    return true
-  }
-
-
-  throw new Error('this type not supported');
+  // null, undefined, NaN
+  return true;
 }
